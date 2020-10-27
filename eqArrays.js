b4d80aeaf2aff78c1567113fd27cdaad5d6de7b1
Indika Kolamba Tantrege <ikolamba2@gmail.com>
2020-10-27 21:59:48 +0000

    Implemented a function that determines the equality between arrays

---
+++ eqArrays.js
@@ -0,0 +1,30 @@
+//Function Implementation
+//Built the string so that it would include quotations for string arguments otherwise it won't
+//Requires npm package node-emoji
+let emoji = require('node-emoji');
+
+const assertEqual = function(actual, expected) {
+  const isEqual = actual === expected;
+  let str = `${isEqual ? emoji.emojify(':100::100::100:Assertion') : emoji.emojify(':poop::poop::poop:Assertion')} ${isEqual ? 'Passed:' : 'Failed:'} ${(typeof actual === 'string') ? `"${actual}"` : actual} ${isEqual ? '===' : '!=='} ${(typeof expected === 'string') ? `"${expected}"` : expected}`;
+  console.log(str);
+};
+
+const eqArrays = (arr1, arr2) => {
+  if(arr1.length === arr2.length){
+    for(let i = 0; i < arr1.length; i++){
+      if(arr1[i] !== arr2[i]){
+        return false;
+      }
+    }
+    return true;
+  }
+  return false;
+};
+
+//Test Code
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2], [1, 2, 3]), false);
+assertEqual(eqArrays([1, 2, 3], [1, 2, 4]), false);
+assertEqual(eqArrays([], [1]), false);
+assertEqual(eqArrays([], []), true);
+
