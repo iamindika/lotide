18361e6ea156c4540bffff3f96fed10e92ef955c
Indika Kolamba Tantrege <ikolamba2@gmail.com>
2020-10-27 22:22:07 +0000

    ASSERTS if two arrays are equal. Implemented within the function call

---
+++ assertArraysEqual.js
@@ -0,0 +1,37 @@
+//Function Implementation
+//Built the string so that it would include quotations for string arguments otherwise it won't
+//Requires npm package node-emoji
+let emoji = require('node-emoji');
+
+const assertEqual = function(actual, expected) {
+  const isEqual = actual === expected;
+  let str = `${isEqual ? emoji.emojify(':100::100::100:Assertion') : emoji.emojify(':poop::poop::poop:Assertion')} ${isEqual ? 'Passed:' : 'Failed:'} ${(typeof actual === 'string') ? `"${actual}"` : actual} ${isEqual ? '===' : '!=='} ${(typeof expected === 'string') ? `"${expected}"` : expected}`;
+  console.log(str);
+};
+
+const eqArrays = (arr1, arr2) => {
+  if(arr1.length === arr2.length){
+    for(let i = 0; i < arr1.length; i++){
+      if(arr1[i] !== arr2[i]){
+        return false;
+      }
+    }
+    return true;
+  }
+  return false;
+};
+
+const assertArrayEqual = (arr1, arr2) => {
+  if(eqArrays(arr1, arr2)){
+    console.log(emoji.emojify(`:100::100::100:Assertion Passed: ${arr1} === ${arr2}`));
+  }else{
+    console.log(emoji.emojify(`:poop::poop::poop:Assertion Failed: ${arr1} === ${arr2}`));
+  }
+};
+//Test Code
+assertArrayEqual([1, 2, 3], [1, 2, 3]);
+assertArrayEqual([1, 2], [1, 2, 3]);
+assertArrayEqual([1, 2, 3], [1, 2, 4]);
+assertArrayEqual([], [1]);
+assertArrayEqual([], []);
+
