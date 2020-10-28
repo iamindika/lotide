187f70eeee207b7a27a153c8c9b1dbf97dc4e681
Indika Kolamba Tantrege <ikolamba2@gmail.com>
2020-10-28 00:15:11 +0000

    Code fixed to pass eslint checks

--- eqArrays.js
+++ eqArrays.js
@@ -10,9 +10,9 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = (arr1, arr2) => {
-  if(arr1.length === arr2.length){
-    for(let i = 0; i < arr1.length; i++){
-      if(arr1[i] !== arr2[i]){
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
         return false;
       }
     }
