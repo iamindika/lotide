210678f773b359f1d937380fb900518d91b706cd
Indika Kolamba Tantrege <ikolamba2@gmail.com>
2020-11-01 04:47:54 +0000

    New function that finds a key for a value that satisfies a user defined callback function

---
+++ findKey.js
@@ -0,0 +1,48 @@
+const findKey = (obj, callback) => {
+  let key;
+  let isKeyFound = false;
+  let objKeys = Object.keys(obj);
+  for (let i = 0; i < objKeys.length && !isKeyFound; i++) {
+    isKeyFound = callback(obj[objKeys[i]]);
+    if (isKeyFound) {
+      key = objKeys[i];
+    }
+  }
+  return key;
+};
+
+let emoji = require('node-emoji');
+
+const assertEqual = function(actual, expected) {
+  const isEqual = actual === expected;
+  let str = `${isEqual ? emoji.emojify(':100::100::100:Assertion') : emoji.emojify(':poop::poop::poop:Assertion')} ${isEqual ? 'Passed:' : 'Failed:'} ${(typeof actual === 'string') ? `"${actual}"` : actual} ${isEqual ? '===' : '!=='} ${(typeof expected === 'string') ? `"${expected}"` : expected}`;
+  console.log(str);
+};
+
+//Test Code
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma");
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 4), undefined);
+assertEqual(findKey({}, x => x.stars === 6), undefined);
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 1), 'Blue Hill');
