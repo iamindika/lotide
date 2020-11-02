0bbad955be8d3a6bf71efa4b3131fd41b5244441
Indika Kolamba Tantrege <ikolamba2@gmail.com>
2020-11-02 18:54:14 +0000

    Refactored code and moved test cases into test library.  Removed assertEquals definition and imported into the relevant test instead.

--- tail.js
+++ tail.js
@@ -1,12 +1,3 @@
-
-let emoji = require('node-emoji');
-
-const assertEqual = function(actual, expected) {
-  const isEqual = actual === expected;
-  let str = `${isEqual ? emoji.emojify(':100::100::100:Assertion') : emoji.emojify(':poop::poop::poop:Assertion')} ${isEqual ? 'Passed:' : 'Failed:'} ${(typeof actual === 'string') ? `"${actual}"` : actual} ${isEqual ? '===' : '!=='} ${(typeof expected === 'string') ? `"${expected}"` : expected}`;
-  console.log(str);
-};
-
 const tail = arr => {
   let tailArr = [];
   for (let i = 1; i < arr.length; i++) {
@@ -15,18 +6,4 @@ const tail = arr => {
   return tailArr;
 };
 
-//Test Code
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
-
-const oneWord = ["Scruffy!"];
-assertEqual(tail(oneWord).length, 0);
-
-const noWords = [];
-assertEqual(tail(noWords).length, 0);
+module.exports = tail;
