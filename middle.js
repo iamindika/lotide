e25811d0deb58db73d15f2f2e518ce8c51b8b97e
Indika Kolamba Tantrege <ikolamba2@gmail.com>
2020-11-02 19:18:29 +0000

    Refactored code. Moved test cases into test library and added imports into test file

--- middle.js
+++ middle.js
@@ -1,34 +1,3 @@
-//Function Implementation
-//Built the string so that it would include quotations for string arguments otherwise it won't
-//Requires npm package node-emoji
-let emoji = require('node-emoji');
-
-// const assertEqual = function(actual, expected) {
-//   const isEqual = actual === expected;
-//   let str = `${isEqual ? emoji.emojify(':100::100::100:Assertion') : emoji.emojify(':poop::poop::poop:Assertion')} ${isEqual ? 'Passed:' : 'Failed:'} ${(typeof actual === 'string') ? `"${actual}"` : actual} ${isEqual ? '===' : '!=='} ${(typeof expected === 'string') ? `"${expected}"` : expected}`;
-//   console.log(str);
-// };
-
-const eqArrays = (arr1, arr2) => {
-  if (arr1.length === arr2.length) {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      }
-    }
-    return true;
-  }
-  return false;
-};
-
-const assertArraysEqual = (arr1, arr2) => {
-  if (eqArrays(arr1, arr2)) {
-    console.log(emoji.emojify(`:100::100::100:Assertion Passed: ${arr1} === ${arr2}`));
-  } else {
-    console.log(emoji.emojify(`:poop::poop::poop:Assertion Failed: ${arr1} === ${arr2}`));
-  }
-};
-
 const middle = arr => {
   let midArrElements = [];
   if (arr.length > 2) {
@@ -42,11 +11,6 @@ const middle = arr => {
   return midArrElements;
 };
 
-//Test Code
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+module.exports = middle;
+
 
