87ba53d39664203e29cf349e5de42273c6ba444d
Indika Kolamba Tantrege <ikolamba2@gmail.com>
2020-11-03 04:05:16 +0000

    Fully utilized mocha and chai for test functions assertions

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,7 +1,21 @@
+const assert = require('chai').assert;
 const assertArraysEqual = require('../assertArraysEqual');
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [1, 2, 4]);
-assertArraysEqual([], [1]);
-assertArraysEqual([], []);
+describe('#assertArraysEqual', function() {
+  it('returns true for [1, 2, 3] and [1, 2, 3]', function() {
+    assert.strictEqual(assertArraysEqual([1, 2, 3], [1, 2, 3]), true);
+  });
+
+  it('returns false for [1, 2, 3] and [1, 2, 4]', function() {
+    assert.strictEqual(assertArraysEqual([1, 2, 3], [1, 2, 4]), false);
+  });
+
+  it('returns false for [] and [1]', function() {
+    assert.strictEqual(assertArraysEqual([], [1]), false);
+  });
+
+  it('returns true for [] and []', function() {
+    assert.strictEqual(assertArraysEqual([], []), true);
+  });
+
+});
