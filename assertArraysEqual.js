0efbff930b1354803b995f1d835d8ed4a6261a1b
Indika Kolamba Tantrege <ikolamba2@gmail.com>
2020-11-03 04:02:44 +0000

    Removed descriptive assertions from assertArraysEqual.js to utilize mocha and chai for that.  Function now just returns true or false

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,21 +1,10 @@
-//Function Implementation
-//Built the string so that it would include quotations for string arguments otherwise it won't
-//Requires npm package node-emoji
-let emoji = require('node-emoji');
-
-// const assertEqual = function(actual, expected) {
-//   const isEqual = actual === expected;
-//   let str = `${isEqual ? emoji.emojify(':100::100::100:Assertion') : emoji.emojify(':poop::poop::poop:Assertion')} ${isEqual ? 'Passed:' : 'Failed:'} ${(typeof actual === 'string') ? `"${actual}"` : actual} ${isEqual ? '===' : '!=='} ${(typeof expected === 'string') ? `"${expected}"` : expected}`;
-//   console.log(str);
-// };
-
 const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = (arr1, arr2) => {
   if (eqArrays(arr1, arr2)) {
-    console.log(emoji.emojify(`:100::100::100:Assertion Passed: ${arr1} === ${arr2}`));
+    return true;
   } else {
-    console.log(emoji.emojify(`:poop::poop::poop:Assertion Failed: ${arr1} !== ${arr2}`));
+    return false;
   }
 };
 
