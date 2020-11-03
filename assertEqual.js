5bccd415984014821f6c3756a28fbaa9850f91df
Indika Kolamba Tantrege <ikolamba2@gmail.com>
2020-11-03 04:17:07 +0000

    Removed descriptive assertions and now only returns true and false

--- assertEqual.js
+++ assertEqual.js
@@ -1,12 +1,5 @@
-//Function Implementation
-//Built the string so that it would include quotations for string arguments otherwise it won't
-//Requires npm package node-emoji
-let emoji = require('node-emoji');
-
 const assertEqual = function(actual, expected) {
-  const isEqual = actual === expected;
-  let str = `${isEqual ? emoji.emojify(':100::100::100:Assertion') : emoji.emojify(':poop::poop::poop:Assertion')} ${isEqual ? 'Passed:' : 'Failed:'} ${(typeof actual === 'string') ? `"${actual}"` : actual} ${isEqual ? '===' : '!=='} ${(typeof expected === 'string') ? `"${expected}"` : expected}`;
-  console.log(str);
+  return actual === expected;
 };
 
 module.exports = assertEqual;
