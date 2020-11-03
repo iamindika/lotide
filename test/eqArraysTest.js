1508827a60ee456d22c6743b52256b92462b7a03
Indika Kolamba Tantrege <ikolamba2@gmail.com>
2020-11-03 04:31:09 +0000

    Placed original assertions within a mocha chai framework

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,8 +1,22 @@
+const assert = require('chai').assert;
 const eqArrays = require('../eqArrays');
-const assertEqual = require('../assertEqual');
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2], [1, 2, 3]), false);
-assertEqual(eqArrays([1, 2, 3], [1, 2, 4]), false);
-assertEqual(eqArrays([], [1]), false);
-assertEqual(eqArrays([], []), true);
+describe('#eqArraysTest', function() {
+  it('returns true for [1, 2, 3] and [1, 2, 3]', function() {
+    assert.strictEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+  });
+
+  it('returns false for [1, 2] and [1, 2, 3]', function() {
+    assert.strictEqual(eqArrays([1, 2], [1, 2, 3]), false);
+  });
+
+  it('returns false for [] and [1]', function() {
+    assert.strictEqual(eqArrays([], [1]), false);
+  });
+
+  it('returns true for [] and []', function() {
+    assert.strictEqual(eqArrays([], []), true);
+  });
+
+});
+
