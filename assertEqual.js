8b2b757609a59585eca2eb9f01909aa4fce35668
Indika Kolamba Tantrege <ikolamba2@gmail.com>
2021-03-29 08:51:57 +0000

    Add test cases and assert console logs
    Add .gitignore for node_modules

--- assertEqual.js
+++ assertEqual.js
@@ -1,5 +1,16 @@
 const assertEqual = function(actual, expected) {
-  return actual === expected;
+  if (actual === expected) {
+    return console.log(`💯💯💯 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    return console.log(`💩💩💩 Assertion Failed: ${actual} !== ${expected}`);
+  }
 };
 
 module.exports = assertEqual;
+
+// Test Code
+
+assertEqual('Lighthouse Labs', 'Juno');
+assertEqual(1, 1);
+assertEqual('1', 1);
+assertEqual('Coding Rocks!', 'Coding Rocks!');
