62b07b9db308d6520f984eb7ad73ddfe3ee9d368
Indika Kolamba Tantrege <ikolamba2@gmail.com>
2021-03-30 20:54:34 +0000

    Create test cases using lotide's assertEqual function

--- head.js
+++ head.js
@@ -1,3 +1,11 @@
+const assertEqual = require('./assertEqual');
+
 const head = arr => arr[0];
 
 module.exports = head;
+
+// Test Cases
+
+// assertEqual(head([3, 2, 1]), 3);
+// assertEqual(head(['beginning', 'end']), 'beginning');
+// assertEqual(head([1, 2, 3, 4]), 2);
