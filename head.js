f7baef7426c7a2ed33d70516832ee9b31ab1882b
Indika Kolamba Tantrege <ikolamba2@gmail.com>
2021-03-30 20:59:53 +0000

    Add 2 more edge cases

--- head.js
+++ head.js
@@ -8,4 +8,6 @@ module.exports = head;
 
 // assertEqual(head([3, 2, 1]), 3);
 // assertEqual(head(['beginning', 'end']), 'beginning');
+// assertEqual(head([5]), 5);
+// assertEqual(head([]), undefined);
 // assertEqual(head([1, 2, 3, 4]), 2);
