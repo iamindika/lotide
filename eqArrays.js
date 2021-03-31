5bb400474e60d1c82d67d9e847925a345f9a7e1b
Indika Kolamba Tantrege <ikolamba2@gmail.com>
2021-03-31 11:46:38 +0000

    Add test cases using lotide's assertEqual function

--- eqArrays.js
+++ eqArrays.js
@@ -1,3 +1,5 @@
+// const assertEqual = require('./assertEqual');
+
 const eqArrays = (arr1, arr2) => {
   if (arr1.length === arr2.length) {
     for (let i = 0; i < arr1.length; i++) {
@@ -12,4 +14,9 @@ const eqArrays = (arr1, arr2) => {
 
 module.exports = eqArrays;
 
+// Test Cases
 
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+// assertEqual(eqArrays([3, 2, 1], [1, 2, 3]), false);
+// assertEqual(eqArrays([1, '2'], [1, 2]), false);
+// assertEqual(eqArrays([], []), true);
