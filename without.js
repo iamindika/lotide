acfd3d63ff508c38c6df33be495fd7f853ddd128
Indika Kolamba Tantrege <ikolamba2@gmail.com>
2021-04-01 09:50:55 +0000

    Improve logic for filtering elements

--- without.js
+++ without.js
@@ -1,31 +1,33 @@
-const eqArrays = require('./eqArrays');
+// const eqArrays = require('./eqArrays');
 
-const assertArraysEqual = require('./assertArraysEqual');
+// const assertArraysEqual = require('./assertArraysEqual');
 
 const without = (source, itemsToRemove) => {
   const newArr = [];
   for (let i = 0; i < source.length; i++) {
     for (let j = 0; j < itemsToRemove.length; j++) {
       if (source[i] === itemsToRemove[j]) {
-          break;
+        break;
       }
 
       if (j === (itemsToRemove.length - 1)) {
-        newArr.push(source[i])
+        newArr.push(source[i]);
       }
     }
   }
   return newArr;
 };
 
+module.exports = without;
+
 //Test Code
-const words = ["hello", "world", "lighthouse"];
-without(words, ["lighthouse"]);
-assertArraysEqual(words, ["hello", "world", "lighthouse"]);
-assertArraysEqual(without([1, 2, 3], [1]), [2, 3]);
-assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
-assertArraysEqual(without([1, 2, 3], [1, 2, 3]), []);
-assertArraysEqual(without([], [1, 2, 3]), []);
-assertArraysEqual(without([], []), []);
-assertArraysEqual(without(['Remove', 'Harry', 'and', 'you', 'will', 'stay'], ['Remove', 'you', 'and']), ['Harry', 'will', 'stay']);
+// const words = ["hello", "world", "lighthouse"];
+// without(words, ["lighthouse"]);
+// assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+// assertArraysEqual(without([1, 2, 3], [1]), [2, 3]);
+// assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
+// assertArraysEqual(without([1, 2, 3], [1, 2, 3]), []);
+// assertArraysEqual(without([], [1, 2, 3]), []);
+// assertArraysEqual(without([], []), []);
+// assertArraysEqual(without(['Remove', 'Harry', 'and', 'you', 'will', 'stay'], ['Remove', 'you', 'and']), ['Harry', 'will', 'stay']);
 
