d4f8862e540055871bb25d491c0315210052987b
Indika Kolamba Tantrege <ikolamba2@gmail.com>
2021-04-03 19:33:59 +0000

    Add alternate solution to middle func and test cases

--- middle.js
+++ middle.js
@@ -1,3 +1,6 @@
+// const assertArrayEquals = require('./assertArraysEqual');
+// const eqArrays = require('./eqArrays');
+
 const middle = arr => {
   let midArrElements = [];
   if (arr.length > 2) {
@@ -9,8 +12,22 @@ const middle = arr => {
     }
   }
   return midArrElements;
+
+  //Alternate solution
+  // const arrLength = arr.length;
+  // const arrMiddle = Math.floor(arr.length / 2);
+  // return (arrLength <= 2) ? [] : ((arrLength % 2 === 0) ? [arr[arrMiddle - 1], arr[arrMiddle]] : [arr[arrMiddle]]);
 };
 
 module.exports = middle;
+
+// Test Cases
+
+// assertArrayEquals(middle([1]), []);
+// assertArrayEquals(middle([1, 2]), []);
+// assertArrayEquals(middle([1, 2, 3]), [2]);
+// assertArrayEquals(middle([1, 2, 3, 4, 5]), [3]);
+// assertArrayEquals(middle([1, 2, 3, 4]), [2, 3]);
+// assertArrayEquals(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
 
 
