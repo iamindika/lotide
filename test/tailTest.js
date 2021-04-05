6a804a472c9d8c88a2a6a98145915c8c676896a7
Indika Kolamba Tantrege <ikolamba2@gmail.com>
2021-04-05 08:00:39 +0000

    Move test code to test files and remove importsfrom originals

--- test/tailTest.js
+++ test/tailTest.js
@@ -12,3 +12,25 @@ describe('#tail', function() {
     assert.deepEqual(tail([]), []);
   });
 });
+
+// Test Cases
+
+// const arr1 = ['Hello', 'from', 'Lighthouse Labs'];
+// const modArr1 = tail(arr1);
+
+// assertEqual(modArr1.length, 2);
+// assertEqual(modArr1[0], 'from');
+// assertEqual(modArr1[1], 'Lighthouse Labs');
+// assertEqual(arr1.length, 3);
+
+// const arr2 = [199];
+// const modArr2 = tail(arr2);
+
+// assertEqual(modArr2.length, 0);
+// assertEqual(arr2.length, 1);
+
+// const arr3 = [];
+// const modArr3 = tail(arr3);
+
+// assertEqual(modArr3.length, 0);
+// assertEqual(arr3.length, 0);
