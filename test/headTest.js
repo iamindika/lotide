e75558393b0e7dec3bf9ca47c657320043ec32dd
Indika Kolamba Tantrege <ikolamba2@gmail.com>
2021-04-05 09:01:58 +0000

    Add a few more test cases for head.js

--- test/headTest.js
+++ test/headTest.js
@@ -5,6 +5,9 @@ describe("#head", () => {
   it("returns 1 for [1, 2, 3]", () => {
     assert.strictEqual(head([1, 2, 3]), 1);
   });
+  it("returns 3 for [3, 2, 1]", () => {
+    assert.strictEqual(head([3, 2, 1]), 3);
+  });
   it("returns '5' for ['5']", () => {
     assert.strictEqual(head(['5']), '5');
   });
@@ -14,15 +17,10 @@ describe("#head", () => {
   it("returns 'Scruffy!' for ['Scruffy!']", () => {
     assert.strictEqual(head(['Scruffy!']), 'Scruffy!');
   });
+  it("returns 'beginning' for ['beginning', 'end']", () => {
+    assert.strictEqual(head(['beginning', 'end']), 'beginning');
+  });
   it("returns undefined for []", () => {
     assert.strictEqual(head([]), undefined);
   });
 });
-
-// Test Cases
-
-// assertEqual(head([3, 2, 1]), 3);
-// assertEqual(head(['beginning', 'end']), 'beginning');
-// assertEqual(head([5]), 5);
-// assertEqual(head([]), undefined);
-// assertEqual(head([1, 2, 3, 4]), 2);
