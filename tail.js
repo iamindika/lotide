371fb6efb6933bdad4a1cc4f66b13a545f2339bd
Indika Kolamba Tantrege <ikolamba2@gmail.com>
2021-04-28 05:43:17 +0000

    Clean up redundant code
    if statement to check length of input array was not neccessary since the for loop's condition contains an implicit length check

--- tail.js
+++ tail.js
@@ -1,10 +1,8 @@
 const tail = arr => {
   let tailArr = [];
 
-  if (arr.length > 1) {
-    for (let i = 1; i < arr.length; i++) {
-      tailArr.push(arr[i]);
-    }
+  for (let i = 1; i < arr.length; i++) {
+    tailArr.push(arr[i]);
   }
 
   return tailArr;
